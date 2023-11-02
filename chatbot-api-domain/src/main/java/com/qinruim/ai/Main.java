f661c9a053b14cb903b38f17b42e17a6379225f3
qrpop <mqinrui@163.com>
2023-11-02 19:31:26 +0800

    初步结构

---
+++ chatbot-api-domain/src/main/java/com/qinruim/ai/Main.java
@@ -0,0 +1,14 @@
+package com.qinruim.ai;
+
+/**
+ * @Package： com.qinruim.ai
+ * @Title: ${NAME}
+ * @Author： qrpop
+ * @Date： 2023-11-02 15:55
+ * @description: ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ chatbot-api-interfaces/src/main/java/com/qinruim/ai/Application.java
@@ -0,0 +1,19 @@
+package com.qinruim.ai;
+
+import org.springframework.boot.SpringApplication;
+
+import javax.swing.*;
+
+/**
+ * @Package： com.qinruim.ai
+ * @Title: ${NAME}
+ * @Author： qrpop
+ * @Date： 2023-11-02 15:57
+ * @description: 启动入口
+ */
+public class Application {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        SpringApplication.run();
+    }
+}
